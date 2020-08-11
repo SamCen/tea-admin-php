f2c45cbd9d1d4cc321720add394617cd6bde0239
sam cen <ryoty123@gmail.com>
2020-08-11 17:48:34 +0800

    bind user

--- app/Http/Controllers/App/Auth/AuthController.php
+++ app/Http/Controllers/App/Auth/AuthController.php
@@ -20,23 +20,22 @@ class AuthController extends Controller
 {
 
 
-
     public function login(LoginRequest $request)
     {
         $credentials = [
-            'username'=>$request->get('username'),
-            'password'=>$request->get('password'),
+            'username' => $request->get('username'),
+            'password' => $request->get('password'),
         ];
 
-        if (! $token = auth('user')->attempt($credentials)) {
-            return error('账号或密码不正确',401);
+        if (!$token = auth('user')->attempt($credentials)) {
+            return error('账号或密码不正确', 401);
         }
         $user = Auth::guard('user')->user();
         $role = $user->role;
-        $user->last_login_ip = isset($_SERVER['HTTP_X_FORWARDED_FOR'])?$_SERVER['HTTP_X_FORWARDED_FOR']:'127.0.0.1';
+        $user->last_login_ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : '127.0.0.1';
         $user->save();
         $response = [
-            'role'=>$role,
+            'role' => $role,
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('user')->factory()->getTTL() * 60
@@ -44,32 +43,32 @@ class AuthController extends Controller
         return success($response);
     }
 
-    public function wechatLogin(WechatLoginRequest $request,Hasher $hasher)
+    public function wechatLogin(WechatLoginRequest $request, Hasher $hasher)
     {
         $code = $request->get('code');
-        $wxService  = new WechatService(config('wechat.wx_appid'),config('wechat.wx_secret'));
+        $wxService = new WechatService(config('wechat.wx_appid'), config('wechat.wx_secret'));
         $res = $wxService->getUserInfoByCode($code);
         $credentials = [
-            'openid'=>$res['openid'],
+            'openid' => $res['openid'],
         ];
         $provider = new EloquentUserProvider($hasher, User::class);
         $user = $provider->retrieveByCredentials($credentials);
-        if(!$user){
+        if (!$user) {
             $response = [
                 'access_token' => null,
-                'code'=>encrypt($code),
+                'code' => encrypt($code),
             ];
-            $cacheKey = sprintf(RedisKey::USER_WXCODE_OPENID,$code);
-            Cache::put($cacheKey,encrypt($res['openid']),600);
-        }else{
-            if (! $token = Auth::guard('user')->login($user)) {
+            $cacheKey = sprintf(RedisKey::USER_WXCODE_OPENID, $code);
+            Cache::put($cacheKey, encrypt($res['openid']), 600);
+        } else {
+            if (!$token = Auth::guard('user')->login($user)) {
                 return error('获取登录凭证失败', 400);
             }
             $role = $user->role;
-            $user->last_login_ip = isset($_SERVER['HTTP_X_FORWARDED_FOR'])?$_SERVER['HTTP_X_FORWARDED_FOR']:'127.0.0.1';
+            $user->last_login_ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : '127.0.0.1';
             $user->save();
             $response = [
-                'role'=>$role,
+                'role' => $role,
                 'access_token' => $token,
                 'token_type' => 'bearer',
                 'expires_in' => auth('user')->factory()->getTTL() * 60
@@ -80,36 +79,35 @@ class AuthController extends Controller
         return success($response);
     }
 
-    public function bindUser(BindUserRequest $request,Hasher $hasher)
+    public function bindUser(BindUserRequest $request, Hasher $hasher)
     {
         $credentials = [
-            'phone'=>$request->get('phone'),
+            'phone' => $request->get('phone'),
         ];
         $code = decrypt($request->get('code'));
-        $cacheKey = sprintf(RedisKey::USER_WXCODE_OPENID,$code);
+        $cacheKey = sprintf(RedisKey::USER_WXCODE_OPENID, $code);
         $openid = decrypt(Cache::pull($cacheKey));
-        if(empty($openid)){
-            return error('code 已失效',400);
+        if (empty($openid)) {
+            return error('code 已失效', 400);
         }
-        Log::info('openid:'.$openid);
         $provider = new EloquentUserProvider($hasher, User::class);
         $user = $provider->retrieveByCredentials($credentials);
-        if(!$user){
+        if (!$user) {
             return error('没有找到手机号对应的用户');
         }
-        if(!empty($user->openid)){
+        if (!empty($user->openid)) {
             return error('该用户已绑定其他微信号了');
         }
-        if (! $token = Auth::guard('user')->login($user)) {
+        if (!$token = Auth::guard('user')->login($user)) {
             return error('绑定失败', 400);
         }
         $role = $user->role;
-        $user->last_login_ip = isset($_SERVER['HTTP_X_FORWARDED_FOR'])?$_SERVER['HTTP_X_FORWARDED_FOR']:'127.0.0.1';
+        $user->last_login_ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : '127.0.0.1';
         $user->openid = $openid;
         $user->save();
 
         $response = [
-            'role'=>$role,
+            'role' => $role,
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('user')->factory()->getTTL() * 60
